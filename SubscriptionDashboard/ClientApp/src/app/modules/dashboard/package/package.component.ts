945f3b4367e36fd87617c1664125ae7bec852bc4
Isaac <itzhak.yohananov@gmail.com>
2023-02-25 14:15:32 +0200

    add package component

---
+++ SubscriptionDashboard/ClientApp/src/app/modules/dashboard/package/package.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-package',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './package.component.html',
+  styleUrls: ['./package.component.css']
+})
+export class PackageComponent {
+
+}
